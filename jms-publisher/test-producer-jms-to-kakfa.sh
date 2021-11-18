6cc3ab6e2d83882baa59d89f8a269cbcb515ef14
Joao Pedro Afonso Cerqueira <jpacerqueira@gmail.com>
2021-11-18 12:58:49 +0000

    Defined optional workaround for when uname is MINGW64_NT-10.0-19042

--- jms-publisher/test-producer-jms-to-kakfa.sh
+++ jms-publisher/test-producer-jms-to-kakfa.sh
@@ -2,10 +2,14 @@
 #
 #java -classpath ~/.m2/repository/javax/jms/jms/1.1/jms-1.1.jar:kafka-jms-fatjar/target/kafka-jms-client-fat-4.1.2.jar:target/jmstokafka-0.0.1.jar com.fuelbigdata.publisher.ProducerApp
 #
-mkdir -p plibjava
-cp -rf  ~/.m2/repository/javax/jms/jms/1.1/jms-1.1.jar plibjava/
-cp -rf kafka-jms-fatjar/target/kafka-jms-client-fat-4.1.2.jar plibjava/
-cp -rf target/jmstokafka-0.0.1.jar plibjava/
-java -cp $(for i in plibjava/*.jar ; do echo -n $i\; ; done).  com.fuelbigdata.publisher.ProducerApp
-rm -rf plibjava
+if [ "$(uname)" == "MINGW64_NT-10.0-19042" ]; then
+    mkdir -p plibjava
+    cp -rf  ~/.m2/repository/javax/jms/jms/1.1/jms-1.1.jar plibjava/
+    cp -rf kafka-jms-fatjar/target/kafka-jms-client-fat-4.1.2.jar plibjava/
+    cp -rf target/jmstokafka-0.0.1.jar plibjava/
+    java -cp $(for i in plibjava/*.jar ; do echo -n $i\; ; done).  com.fuelbigdata.publisher.ProducerApp
+    rm -rf plibjava
+else
+    java -classpath ~/.m2/repository/javax/jms/jms/1.1/jms-1.1.jar:kafka-jms-fatjar/target/kafka-jms-client-fat-4.1.2.jar:target/jmstokafka-0.0.1.jar com.fuelbigdata.publisher.ProducerApp
+fi
 #
--- jms-publisher/test-publisher-jms-to-kakfa.sh
+++ jms-publisher/test-publisher-jms-to-kakfa.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 #
 # java -classpath ~/.m2/repository/javax/jms/jms/1.1/jms-1.1.jar:kafka-jms-fatjar/target/kafka-jms-client-fat-4.1.2.jar:target/jmstokafka-0.0.1.jar com.fuelbigdata.publisher.PublisherApp
-mkdir -p libjava
-cp -rf ~/.m2/repository/javax/jms/jms/1.1/jms-1.1.jar libjava/
-cp -rf kafka-jms-fatjar/target/kafka-jms-client-fat-4.1.2.jar libjava/ 
-cp -rf target/jmstokafka-0.0.1.jar libjava/
-java -cp $(for i in libjava/*.jar ; do echo -n $i\; ; done).  com.fuelbigdata.publisher.PublisherApp
-rm -rf libjava
+#
+if [ "$(uname)" == "MINGW64_NT-10.0-19042" ]; then
+	mkdir -p libjava
+	cp -rf ~/.m2/repository/javax/jms/jms/1.1/jms-1.1.jar libjava/
+	cp -rf kafka-jms-fatjar/target/kafka-jms-client-fat-4.1.2.jar libjava/ 
+	cp -rf target/jmstokafka-0.0.1.jar libjava/
+	java -cp $(for i in libjava/*.jar ; do echo -n $i\; ; done).  com.fuelbigdata.publisher.PublisherApp
+	rm -rf libjava
+else
+	java -classpath ~/.m2/repository/javax/jms/jms/1.1/jms-1.1.jar:kafka-jms-fatjar/target/kafka-jms-client-fat-4.1.2.jar:target/jmstokafka-0.0.1.jar com.fuelbigdata.publisher.PublisherApp
+fi
 #
