cd665992938d34583c08b704bdaf27abfcd36eec
Joao Pedro Afonso Cerqueira <jpacerqueira@gmail.com>
2021-11-18 12:47:57 +0000

    exampel run for WSL2 gitbash linux with windows command line workarround for java

--- jms-publisher/test-producer-jms-to-kakfa.sh
+++ jms-publisher/test-producer-jms-to-kakfa.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 #
-java -classpath ~/.m2/repository/javax/jms/jms/1.1/jms-1.1.jar:kafka-jms-fatjar/target/kafka-jms-client-fat-4.1.2.jar:target/jmstokafka-0.0.1.jar com.fuelbigdata.publisher.ProducerApp
+#java -classpath ~/.m2/repository/javax/jms/jms/1.1/jms-1.1.jar:kafka-jms-fatjar/target/kafka-jms-client-fat-4.1.2.jar:target/jmstokafka-0.0.1.jar com.fuelbigdata.publisher.ProducerApp
+#
+mkdir -p plibjava
+cp -rf  ~/.m2/repository/javax/jms/jms/1.1/jms-1.1.jar plibjava/
+cp -rf kafka-jms-fatjar/target/kafka-jms-client-fat-4.1.2.jar plibjava/
+cp -rf target/jmstokafka-0.0.1.jar plibjava/
+java -cp $(for i in plibjava/*.jar ; do echo -n $i\; ; done).  com.fuelbigdata.publisher.ProducerApp
+rm -rf plibjava
 #
--- jms-publisher/test-publisher-jms-to-kakfa.sh
+++ jms-publisher/test-publisher-jms-to-kakfa.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 #
-java -classpath ~/.m2/repository/javax/jms/jms/1.1/jms-1.1.jar:kafka-jms-fatjar/target/kafka-jms-client-fat-4.1.2.jar:target/jmstokafka-0.0.1.jar com.fuelbigdata.publisher.PublisherApp
+# java -classpath ~/.m2/repository/javax/jms/jms/1.1/jms-1.1.jar:kafka-jms-fatjar/target/kafka-jms-client-fat-4.1.2.jar:target/jmstokafka-0.0.1.jar com.fuelbigdata.publisher.PublisherApp
+mkdir -p libjava
+cp -rf ~/.m2/repository/javax/jms/jms/1.1/jms-1.1.jar libjava/
+cp -rf kafka-jms-fatjar/target/kafka-jms-client-fat-4.1.2.jar libjava/ 
+cp -rf target/jmstokafka-0.0.1.jar libjava/
+java -cp $(for i in libjava/*.jar ; do echo -n $i\; ; done).  com.fuelbigdata.publisher.PublisherApp
+rm -rf libjava
 #
