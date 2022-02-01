ac3cf5247f6cd12980c1af8464c03fe2d4bb52d5
Joao Pedro Afonso Cerqueira <jpacerqueira@gmail.com>
2022-02-01 13:08:26 +0000

    included GitBash updated versionId to MINGW64_NT-10.0-19044 ,  for scripting in Grads training

--- jms-publisher/test-publisher-jms-to-kakfa.sh
+++ jms-publisher/test-publisher-jms-to-kakfa.sh
@@ -2,7 +2,7 @@
 #
 # java -classpath ~/.m2/repository/javax/jms/jms/1.1/jms-1.1.jar:kafka-jms-fatjar/target/kafka-jms-client-fat-4.1.2.jar:target/jmstokafka-0.0.1.jar com.fuelbigdata.publisher.PublisherApp
 #
-if [ "$(uname)" == "MINGW64_NT-10.0-19042" ]; then
+if [ "$(uname)" == "MINGW64_NT-10.0-19044" ]; then
 	mkdir -p libjava
 	cp -rf ~/.m2/repository/javax/jms/jms/1.1/jms-1.1.jar libjava/
 	cp -rf kafka-jms-fatjar/target/kafka-jms-client-fat-4.1.2.jar libjava/ 
