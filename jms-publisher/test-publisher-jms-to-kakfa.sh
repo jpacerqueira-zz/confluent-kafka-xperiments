38e2d0d1fff0249f1d105a6312a05b6a31509641
jpacerqueira <jpacerqueira@gmail.com>
2022-02-17 13:30:24 +0000

    Generic pattern matching to identify OS of env for producer/consumer bash scripts

--- jms-publisher/test-publisher-jms-to-kakfa.sh
+++ jms-publisher/test-publisher-jms-to-kakfa.sh
@@ -2,7 +2,8 @@
 #
 # java -classpath ~/.m2/repository/javax/jms/jms/1.1/jms-1.1.jar:kafka-jms-fatjar/target/kafka-jms-client-fat-4.1.2.jar:target/jmstokafka-0.0.1.jar com.fuelbigdata.publisher.PublisherApp
 #
-if [ "$(uname)" == "MINGW64_NT-10.0-19044" ]; then
+PATTERN_X="^(MINGW64_NT-10.0-19044$|MINGW64_NT-10.*-*)"
+if [[ $(uname) =~ $PATTERN_X ]]; then
 	mkdir -p libjava
 	cp -rf ~/.m2/repository/javax/jms/jms/1.1/jms-1.1.jar libjava/
 	cp -rf kafka-jms-fatjar/target/kafka-jms-client-fat-4.1.2.jar libjava/ 
